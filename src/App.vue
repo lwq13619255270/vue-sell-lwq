fea7000cf6af5193cd19e4a2b03f59176a2b58cf
刘伟奇 <13619255270@163.com>
2024-04-01 23:48:01 +0800

    封装tab组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
   import { getSeller } from 'api'
   import VHeader from 'components/v-header/v-header'
+  import Tab from 'components/tab/tab'
+  import Goods from 'components/goods/goods'
+  import Ratings from 'components/ratings/ratings'
+  import Seller from 'components/seller/seller'
   export default {
     name: 'App',
     data() {
@@ -24,12 +31,46 @@
         })
       }
     },
+    computed: {
+      tabs() {
+        return [
+          {
+            label: '商品',
+            component: Goods,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label: '评论',
+            component: Ratings,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label: '商家',
+            component: Seller,
+            data: {
+              seller: this.seller
+            }
+          }
+        ]
+      }
+    },
     components: {
-      VHeader
+      VHeader,
+      Tab
     }
   }
 </script>
 
 <style lang="stylus">
-
+ #app
+  .tab-wrapper
+    position: fixed
+    top: 136px
+    left: 0
+    right: 0
+    bottom: 0
 </style>
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="tab">
+		<cube-tab-bar
+			ref="tabBar"
+			:use-transition= false
+			:showSlider = true
+			:data="tabs"
+			v-model="selectedLabel"
+		>
+		</cube-tab-bar>
+		<div class="slide-wrapper">
+			<cube-slide
+				ref="slide"
+				:initial-index="index"
+				:loop="false"
+				:auto-play="false"
+				:show-dots=false
+				:options="slideOptions"
+				@scroll="onScroll"
+				@change="onChange">
+				<cube-slide-item v-for="(tab, index) in tabs" :key="index">
+					<component ref="component" :is="tab.component" :data="tab.data"></component>
+				</cube-slide-item>
+			</cube-slide>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'tab',
+		props: {
+			tabs: {
+				type: Array,
+				default() {
+					return []
+				}
+			},
+			initialIndex: {
+				type: Number,
+				default: 0
+			}
+		},
+		data () {
+			return {
+				index: this.initialIndex,
+				slideOptions: {
+          listenScroll: true,
+          probeType: 3,
+          directionLockThreshold: 0
+        }
+			}
+		},
+		computed: {
+			selectedLabel: {
+				get () {
+					return this.tabs[this.index].label
+				},
+				set(newVal) {
+					this.index = this.tabs.findIndex(value => {
+						return value.label === newVal
+					})
+				}
+			}
+		},
+		methods: {
+			onScroll(pos) {
+        const tabBarWidth = this.$refs.tabBar.$el.clientWidth
+        const slideWidth = this.$refs.slide.slide.scrollerWidth
+        const transform = -pos.x / slideWidth * tabBarWidth
+        this.$refs.tabBar.setSliderTransform(transform)
+      },
+			onChange(current) {
+        this.index = current
+        const instance = this.$refs.component[current]
+        if (instance && instance.fetch) {
+          instance.fetch()
+        }
+      }
+		}
+}
+</script>
+<style lang="stylus" scoped>
+	@import "~common/stylus/variable"
+
+	.tab
+		display: flex
+		flex-direction: column
+		height: 100%
+		>>> .cube-tab
+			padding: 10px 0
+		.slide-wrapper
+			flex: 1
+			overflow: hidden
+</style>
